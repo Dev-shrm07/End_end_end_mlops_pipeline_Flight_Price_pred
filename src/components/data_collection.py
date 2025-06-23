7aa8749e8a1bc2998a7b8b5c4b6d444bc0464b92
kavyadaddy <yobrochill56@gmail.com>
2025-06-23 15:40:28 +0530

    changes

---
+++ src/components/data_collection.py
@@ -0,0 +1,9 @@
+
+"""Definition of classes or methods to get data from whichever source it is in"""
+
+class GET_DATA:
+    def __init__(self):
+        pass
+    
+    def get_data(self):
+        pass
