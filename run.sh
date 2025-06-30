a411611c2f22f2cc27dadb4cb3a2c572099d4975
kavyadaddy <yobrochill56@gmail.com>
2025-06-30 17:12:34 +0530

    ecw

--- run.sh
+++ run.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 
-mlflow ui --host 0.0.0.0 --port 5000 &
-MLFLOW_UI_PID=$!
-echo "MLflow started! Webserver: http://localhost:5000"
+# mlflow ui --host 0.0.0.0 --port 5000 &
+# MLFLOW_UI_PID=$!
+# echo "MLflow started! Webserver: http://localhost:5000"
 
 
-export AIRFLOW_HOME=$(pwd)/airflow
-export AIRFLOW__CORE__LOAD_EXAMPLES=False
-export AIRFLOW__CORE__AUTH_MANAGER=airflow.providers.fab.auth_manager.fab_auth_manager.FabAuthManager
+# export AIRFLOW_HOME=$(pwd)/airflow
+# export AIRFLOW__CORE__LOAD_EXAMPLES=False
+# export AIRFLOW__CORE__AUTH_MANAGER=airflow.providers.fab.auth_manager.fab_auth_manager.FabAuthManager
 
-airflow db migrate
+# airflow db migrate
 
-airflow users create \
-  --username airflow \
-  --firstname Airflow \
-  --lastname Admin \
-  --email airflow@example.com \
-  --role Admin \
-  --password "$AIRFLOW_PASSWORD"   
+# airflow users create \
+#   --username airflow \
+#   --firstname Airflow \
+#   --lastname Admin \
+#   --email airflow@example.com \
+#   --role Admin \
+#   --password "$AIRFLOW_PASSWORD"   
 
 
-airflow api-server --host 0.0.0.0 --port 8080 &
-AIRFLOW_WEBSERVER_PID=$!
-airflow scheduler &
-AIRFLOW_SCHEDULER_PID=$!
-airflow dag-processor &
-AIRFLOW_DAG_PROCESSOR_PID=$!
-echo "Airflow started! Webserver: http://localhost:8080"
-echo "DAGs folder: $AIRFLOW_HOME/dags"
+# airflow api-server --host 0.0.0.0 --port 8080 &
+# AIRFLOW_WEBSERVER_PID=$!
+# airflow scheduler &
+# AIRFLOW_SCHEDULER_PID=$!
+# airflow dag-processor &
+# AIRFLOW_DAG_PROCESSOR_PID=$!
+# echo "Airflow started! Webserver: http://localhost:8080"
+# echo "DAGs folder: $AIRFLOW_HOME/dags"
 
 
 uvicorn api.app:app --host 0.0.0.0 --port 8000 &
